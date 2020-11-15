5a6c6b4ae4ef2e0d5b46bcfa249e5fe6bfc033c5
ioni90 <kkoioni27@gmail.com>
2020-11-15 16:32:23 +0900

    bids,orderコントローラー調整

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,4 +1,5 @@
 class BidsController < ApplicationController
+  before_action :bid_info, only: [:show]
 
   def new
     @order = Order.find(params[:order_id])
@@ -10,19 +11,28 @@ class BidsController < ApplicationController
       @bid.save
       redirect_to root_path
     else
+      @order = Order.find(params[:order_id])
       render :new
     end
   end
 
+  def show
+    @order = Order.find(params[:order_id])
+  end
+
 private
 
-def bid_params
-  params.permit( :price_ok, :size_ok, :material_ok, 
-                               :detail_ok, :delivery_time_ok,
-                               :price_remark, :size_remark, :material_remark,
-                               :detail_remark, 
-                               :delivery_time_remark, :message, 
-                               :order_id).merge(user_id: current_user.id)
-end
+  def bid_params
+    params.permit(:offer_price, :price_ok, :size_ok, :material_ok, 
+                                :detail_ok, :delivery_time_ok,
+                                :price_remark, :size_remark, :material_remark,
+                                :detail_remark, 
+                                :delivery_time_remark, :message, 
+                                :order_id).merge(user_id: current_user.id)
+  end
+
+  def bid_info
+    @bid = Bid.find(params[:id])
+  end
 
 end
---
+++ spec/requests/closures_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Closures", type: :request do
+
+end
